2d3e04ef4098885a9fc7b3827dcd818d87b68f3b
vitoturing <cuccovito@gmail.com>
2017-10-17 14:29:58 +0200

    Building structure

--- src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/training/utils/WekaModelHandler.java
+++ src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/training/utils/WekaModelHandler.java
@@ -3,29 +3,28 @@ package com.github.swapUniba.pulse.crowd.machinelearning.training.utils;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import weka.classifiers.Classifier;
 
-
+/***
+ * Salva e carica i modelli dei classificatori di Weka
+ */
 public class WekaModelHandler {
 
-    public static void main(String[] args) throws Exception{
+    public static final String curPath = System.getProperty("user.dir") + "//models//";
 
-        SaveModel("cazzarola","Strunz");
-
-        }
 
     public static void SaveModel(String filename, Object predictiveModel) throws Exception {
-        String curPath = System.getProperty("user.dir") + "//models//";
+
         File f = new File(curPath);
         if (!f.exists() && !f.isDirectory()) {
             Files.createDirectory(Paths.get(curPath));
         }
+
         weka.core.SerializationHelper.write(curPath  + filename+".model", predictiveModel);
     }
 
     public static Classifier LoadModel(String filename) throws Exception {
-        Classifier cls = (Classifier) weka.core.SerializationHelper.read("C://models//"+filename+".model");
+        Classifier cls = (Classifier) weka.core.SerializationHelper.read(curPath + filename + ".model");
         return cls;
     }
 
