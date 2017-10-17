fd0ff49b2496bb9b14f16a078164ceaf8bf8f629
vitoturing <cuccovito@gmail.com>
2017-10-17 19:47:23 +0200

    MessagesToWeka

--- src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/training/TrainModel.java
+++ src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/training/TrainModel.java
@@ -55,36 +55,6 @@ public class TrainModel {
             WekaModelHandler.SaveModel(config.getModelName(), algorithm); //salvare il modello con il suo nome
             classifierBuilt = true;
 
-/*            List<MongoWekaDTO> collectionList = new ArrayList<>();
-
-            for (String coll : _collectionNames){
-                MongoWekaDTO mongoWekaDTO = CollectionConverter.getDatasetFromMongo(_dbname, coll,_collectionNames);
-                 collectionList.add(mongoWekaDTO);
-            }
-
-            Instances trainingSet = CollectionConverter.mergeInstances(collectionList, config.getModelName());
-            trainingSet.setClassIndex(trainingSet.numAttributes() - 1); //l'attributo di classe è sempre l'ultimo che aggiungo
-            AbstractClassifier algorithm = null;
-
-            if (MLAlgorithmEnum.valueOf(config.getAlgorithm()) == MLAlgorithmEnum.J48) {
-                 algorithm = new J48();
-            }
-
-            if (MLAlgorithmEnum.valueOf(config.getAlgorithm()) == MLAlgorithmEnum.NaiveBayes) {
-                 algorithm = new NaiveBayes();
-            }
-
-            if (MLAlgorithmEnum.valueOf(config.getAlgorithm()) == MLAlgorithmEnum.LinearRegression) {
-                 algorithm = new LinearRegression();
-            }
-
-            String[] options = weka.core.Utils.splitOptions(config.getAlgorithmParams());
-            algorithm.setOptions(options);
-            algorithm.buildClassifier(trainingSet);
-            System.out.println("Model has been built!");
-            WekaModelHandler.SaveModel(config.getModelName(), algorithm); //salvare il modello con il suo nome
-            classifierBuilt = true;*/
-
         }
         catch (Exception ex) {
             MachineLearningTrainingPlugin.logger.error(ex.toString());
