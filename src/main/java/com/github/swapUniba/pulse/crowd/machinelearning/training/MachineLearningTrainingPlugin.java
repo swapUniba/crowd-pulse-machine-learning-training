a63dde988b87dd04153263314d2a3176beb45192
vitoturing <cuccovito@gmail.com>
2017-10-17 12:22:14 +0200

    Initial Commit

--- src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/training/MachineLearningTrainingPlugin.java
+++ src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/training/MachineLearningTrainingPlugin.java
@@ -2,18 +2,23 @@ package com.github.swapUniba.pulse.crowd.machinelearning.training;
 
 import com.github.frapontillo.pulse.crowd.data.entity.Message;
 import com.github.frapontillo.pulse.spi.IPlugin;
+import com.github.frapontillo.pulse.util.PulseLogger;
+import org.apache.logging.log4j.Logger;
 import rx.Observable;
 
 public class MachineLearningTrainingPlugin extends IPlugin<Message,Message,MachineLearningTrainingConfig> {
 
+    private static final String PLUGIN_NAME = "machine-learning-training";
+    public static final Logger logger = PulseLogger.getLogger(MachineLearningTrainingPlugin.class);
+
     @Override
     public String getName() {
-        return null;
+        return PLUGIN_NAME;
     }
 
     @Override
     public MachineLearningTrainingConfig getNewParameter() {
-        return null;
+        return new MachineLearningTrainingConfig();
     }
 
     @Override
