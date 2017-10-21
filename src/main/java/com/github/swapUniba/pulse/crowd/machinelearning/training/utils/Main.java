a8a0d9d0a7f810d7f36965282d6e3a5087f56c47
vitoturing <cuccovito@gmail.com>
2017-10-21 17:58:57 +0200

    MessagesToWeka

--- src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/training/utils/Main.java
+++ src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/training/utils/Main.java
@@ -12,8 +12,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Message> msgs = new ArrayList<>();
+        boolean testClassification = false;
 
+        if (testClassification) {
+            TestMessageClassification();
+        }
+        else {
+            //TestRegression();
+        }
+    }
+
+
+    private static void TestMessageClassification() {
+
+        List<Message> msgs = new ArrayList<>();
 
         for (int i = 0; i < 100; i++) {
 
